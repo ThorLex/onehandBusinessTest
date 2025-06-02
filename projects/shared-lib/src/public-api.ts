3f8d614c7994732fda36717a637387c800f59745
beyas <beyasbekono@gmail.com>
2025-06-02 04:47:53 +0100

    push(#5): debut design

--- projects/shared-lib/src/public-api.ts
+++ projects/shared-lib/src/public-api.ts
@@ -2,4 +2,11 @@
  * Public API Surface of shared-lib
  */
 
-export * from './lib/shared-lib';
+export * from './lib/component/home/home';
+export * from './lib/component/form/form';
+export * from './lib/service';
+export * from './lib/dto/inputs';
+export * from './lib/dto/outputs';
+export * from './lib/store/action';
+export * from './lib/store/state/person.state';
+export * from './lib/store/state/todo.state';
