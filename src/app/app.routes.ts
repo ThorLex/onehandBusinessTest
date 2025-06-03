5d16237c1bd2d6e0823c5bf5bba15de802320cde
beyas <beyasbekono@gmail.com>
2025-06-03 16:42:15 +0100

    save

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FormComponent } from './components/form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'form', component: FormComponent }
+];
